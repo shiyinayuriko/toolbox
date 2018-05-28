aea1c272fe4889fa6a1bab308e7537354138965e
shiyinayuriko <whitecometliliwill@gmail.com>
2018-05-29 01:36:42 +0800

    first version for markdown image uploader

---
+++ markdown_uploader/launcher.py
@@ -0,0 +1,51 @@
+import re
+import lychee
+import requests
+import sys
+import os
+
+reg = re.compile(r'!\[([^\]]*)\]\(([^\)]*)\)')
+httpCheck = re.compile(r'(https?|ftp|file)://[-A-Za-z0-9+&@  # /%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]')
+
+
+def dealwithFile(sourceFile):
+    uploader = lychee.lychee("shiyinayuriko", "*", albumName="啊啊是")
+    dirname = os.path.dirname(sourceFile)
+    tartgetFile = os.path.splitext(sourceFile)[0] + "_out" + os.path.splitext(sourceFile)[1]
+
+    def imageRepl(match):
+        matchStr = match.group()
+
+        picUrl = match.group(2)
+        if(httpCheck.match(picUrl)):
+            print("already Url skip:", match.group(2))
+            return matchStr
+
+        if(not os.path.isfile(picUrl)):
+            picUrl = dirname + picUrl
+
+        if(not os.path.isfile(picUrl)):
+            print("not exchange file not exist:", match.group(2))
+            return matchStr
+            
+        try:
+            targetUrl = uploader.getUrlStr(picUrl)
+        except Exception:
+            print("not exchange upload fail:", match.group(2))
+            return matchStr
+
+        if(httpCheck.match(targetUrl)):
+            print("exchange:", match.group(2), " -> ", targetUrl)
+            return matchStr.replace(match.group(2), targetUrl)
+        else:
+            print("not exchange upload url error:", match.group(2),  " X-X ", targetUrl)
+            return matchStr
+
+    with open(sourceFile, "r", encoding="utf8") as f, open(tartgetFile, "w", encoding="utf8") as f2:
+        for line in f:
+            newLine = reg.sub(imageRepl, line)
+            f2.write(newLine)
+
+
+sourceFile = sys.argv[1]
+dealwithFile(sourceFile)
---
+++ markdown_uploader/lychee.py
@@ -0,0 +1,91 @@
+import requests
+# pip install cryptography
+# pip install pyOpenSSL
+# pip install certifi
+
+import os
+
+
+class lychee:
+    # session
+    baseUrl = "*"
+    baseIndexUrl = baseUrl + "/php/index.php"
+
+    albumIDCache = {}
+    photoUrlCache = {}
+
+    def __init__(self, userName, password, albumName="defaultAlbum"):
+        self.albumName = albumName
+        self.userName = userName
+        self.password = password
+        self.session = requests.Session()
+        self.__login()
+
+    def __login(self):
+        payload = {'function': 'Session::login', "password": self.password, "user": self.userName}
+        r = self.session.post(self.baseIndexUrl, data=payload)
+        if(not r.json()):
+            raise Exception("password error")
+
+
+
+
+    def getUrlStr(self, localUrl):
+        if(self.photoUrlCache.__contains__(localUrl)):
+            return self.photoUrlCache[localUrl]
+
+        url = self.__upload(localUrl, self.albumName)
+        self.albumIDCache[localUrl] = url
+
+        return url
+
+    def __upload(self, localUrl, albumName):
+        albumId = self.getAlbumId(albumName)
+        payload = {'function': 'Photo::add', "albumID": albumId}
+        # os.path.basename(localUrl)
+        # 中文文件名在requests发包时会出错
+        fileJson = {"0": ("fileName" + os.path.splitext(localUrl)[1], open(localUrl, 'rb'))}
+
+        r = self.session.post(self.baseIndexUrl, data=payload, files=fileJson)
+        if(r.json()):
+            photoId = r.json()
+            payload = {'function': 'Photo::get', 'albumID': albumId, "photoID": photoId}
+            r = self.session.post(self.baseIndexUrl, data=payload)
+            return self.baseUrl + "/" + r.json()["url"]
+        else:
+            print("upload fail")
+            return localUrl
+
+    def getAlbumId(self, albumName, isAutoCreate=True):
+        if(self.albumIDCache.__contains__(albumName)):
+            return self.albumIDCache[albumName]
+
+        try:
+            payload = {'function': 'Albums::get'}
+            r = self.session.post(self.baseIndexUrl, data=payload)
+            jsonResp = r.json()
+            for album in jsonResp['albums']:
+                self.albumIDCache[album["title"]] = album["id"]
+        except AttributeError:
+            print("jsonError")
+        except KeyError:
+            print("jsonError")
+        except TypeError:
+            print("jsonError")
+        finally:
+            pass
+
+        if(self.albumIDCache.__contains__(albumName)):
+            return self.albumIDCache[albumName]
+
+        if(not isAutoCreate):
+            return 0
+
+        payload = {'function': 'Album::add', 'title': albumName}
+        r = self.session.post(self.baseIndexUrl, data=payload)
+        if(r.json() is int):
+            self.albumIDCache[albumName] = r.json()
+            return r.json()
+
+        raise Exception("create album error")
+
