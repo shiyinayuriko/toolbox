475fa13a3ff71df4086bf79ff49f787c10056070
shiyinayuriko <whitecometliliwill@gmail.com>
2018-05-30 01:48:05 +0800

    add terminal interaction

--- markdown_uploader/launcher.py
+++ markdown_uploader/launcher.py
@@ -7,6 +7,8 @@ import os
 reg = re.compile(r'!\[([^\]]*)\]\(([^\)]*)\)')
 httpCheck = re.compile(r'(https?|ftp|file)://[-A-Za-z0-9+&@  # /%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]')
 
+# success skip fail
+counter = [0,0,0]
 
 def dealwithFile(sourceFile):
     dirname = os.path.dirname(sourceFile)
@@ -21,6 +23,7 @@ def dealwithFile(sourceFile):
         picUrl = match.group(2)
         if(httpCheck.match(picUrl)):
             print("already Url skip:", match.group(2))
+            counter[1] += 1
             return matchStr
 
         if(not os.path.isfile(picUrl)):
@@ -28,19 +31,23 @@ def dealwithFile(sourceFile):
 
         if(not os.path.isfile(picUrl)):
             print("not exchange file not exist:", match.group(2))
+            counter[2] += 1
             return matchStr
 
         try:
             targetUrl = uploader.getUrlStr(picUrl)
         except Exception:
             print("not exchange upload fail:", match.group(2))
+            counter[2] += 1
             return matchStr
 
         if(httpCheck.match(targetUrl)):
             print("exchange:", match.group(2), " -> ", targetUrl)
+            counter[0] += 1
             return matchStr.replace(match.group(2), targetUrl)
         else:
             print("not exchange upload url error:", match.group(2),  " X-X ", targetUrl)
+            counter[2] += 1
             return matchStr
 
     with open(sourceFile, "r", encoding="utf8") as f, open(tartgetFile, "w", encoding="utf8") as f2:
@@ -49,5 +56,11 @@ def dealwithFile(sourceFile):
             f2.write(newLine)
 
 
-sourceFile = sys.argv[1]
+if(sys.argv.__len__() == 1):
+    sourceFile = input("input source file\n")
+else:
+    sourceFile = sys.argv[1]
+
 dealwithFile(sourceFile)
+print("%d successed, %d skipped, %d failed" %(counter[0], counter[1], counter[2]))
+input("end\n")
